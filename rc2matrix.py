7cd309ca90429899d0b2e2846dc38a5821e1267a
Francois Lesueur <francois.lesueur@worteks.com>
2023-08-09 14:55:52 +0200

    handles images, files, citations and markdown formatting

--- rc2matrix.py
+++ rc2matrix.py
@@ -8,6 +8,7 @@ import json
 import requests
 from datetime import datetime
 import re
+import markdown
 
 
 # globals
@@ -53,10 +54,11 @@ def format_message(raw, ancestor=None):
     if ancestor is not None:
         return relate_message(raw, ancestor)
 
-    formatted = raw
-    formatted = re.sub("```(.+)```", "<code>\\1</code>", formatted)
-    formatted = re.sub("`(.+)`", "<code>\\1</code>", formatted)
-    if formatted == raw:
+    #formatted = raw
+    #formatted = re.sub("```(.+)```", "<code>\\1</code>", formatted)
+    #formatted = re.sub("`(.+)`", "<code>\\1</code>", formatted)
+    formatted = markdown.markdown(raw)
+    if len(formatted) <= len(raw)+7: # markdown adds <p></p> tags
         api_params = {'msgtype': 'm.text', 'body': raw}
     else:
         api_params = {'msgtype': 'm.text', 'body': raw,
@@ -168,6 +170,7 @@ if __name__ == '__main__':
         for line in jsonfile:
             currentmsg = json.loads(line)
             pprint("current message", currentmsg)
+            finished=False
             if currentmsg['rid'] in roomids:
                 tgtroom = roomids[currentmsg['rid']]
                 tgtuser = "@" + currentmsg['u']['username'] + ":" + args.hostname
@@ -176,93 +179,135 @@ if __name__ == '__main__':
                 if tgtts < lastts: # messages are not sorted, bad things will happen
                     exit("Messages are not sorted, leaving...")
                 lastts = tgtts
-                # vprint("should be in room " + str(tgtroom))
-                # Post message
-                if 'file' in currentmsg: # File upload
-                    # "file":{"_id":"u5Ga3vn36LCT9bfhW","name":"tree-736885_640.jpg","type":"image/jpeg"}
-                    api_endpoint = api_base + "/_matrix/media/v3/upload?user_id=" + tgtuser + "&ts=" + str(tgtts)
-                    api_params = {'filename': currentmsg['file']['name']}
-                    #files = {'file': open('inputs/files/u5Ga3vn36LCT9bfhW', 'rb')}
-                    api_headers_file = api_headers_as
-                    api_headers_file['Content-Type'] = currentmsg['file']['type']
-                    try:
-                        with open("inputs/files/" + currentmsg['file']['_id'], 'rb') as f:
-                            response = requests.post(api_endpoint, json=api_params, headers=api_headers_file, data=f)
-                        vprint(response.json())
-                        mxcurl=response.json()['content_uri']
-                        api_endpoint = api_base + "_matrix/client/v3/rooms/" + tgtroom + '/send/m.room.message?user_id=' + tgtuser + "&ts=" + str(tgtts) # ts, ?user_id=@_irc_user:example.org
-                        api_params = {'msgtype': 'm.file', 'body': currentmsg['file']['name'], 'url': mxcurl}
-                        response = requests.post(api_endpoint, json=api_params, headers=api_headers_as)
-                        vprint(response.json())
-                    except FileNotFoundError:
-                        api_endpoint = api_base + "_matrix/client/v3/rooms/" + tgtroom + '/send/m.room.message?user_id=' + tgtuser + "&ts=" + str(tgtts) # ts, ?user_id=@_irc_user:example.org
-                        api_params = {'msgtype': 'm.text', 'body': "<<< A file named \"" + currentmsg['file']['name'] + "\" was lost during the migration to Matrix >>>"}
-                        response = requests.post(api_endpoint, json=api_params, headers=api_headers_as)
-                        vprint(response.json())
-                else: # standard message
+                # First, iterate attachments
+                if 'attachments' in currentmsg:
+                    for attachment in currentmsg['attachments']:
+                        if 'type' in attachment and attachment['type'] == 'file': # A file
+                            vprint("a file")
+                            api_endpoint = api_base + "/_matrix/media/v3/upload?user_id=" + tgtuser + "&ts=" + str(tgtts)
+                            api_params = {'filename': attachment['title']}
+                            #files = {'file': open('inputs/files/u5Ga3vn36LCT9bfhW', 'rb')}
+                            api_headers_file = api_headers_as.copy()
+                            if 'image_type' in attachment:
+                                vprint("an image")
+                                api_headers_file['Content-Type'] = attachment['image_type']
+                            try:
+                                localfile=attachment['title_link']
+                                localfile=re.sub("/file-upload/", "", localfile)
+                                localfile=re.sub("/.*", "", localfile)
+                                with open("inputs/files/" + localfile, 'rb') as f:
+                                    response = requests.post(api_endpoint, json=api_params, headers=api_headers_file, data=f)
+                                vprint(response.json())
+                                mxcurl=response.json()['content_uri']
+                                api_endpoint = api_base + "_matrix/client/v3/rooms/" + tgtroom + '/send/m.room.message?user_id=' + tgtuser + "&ts=" + str(tgtts) # ts, ?user_id=@_irc_user:example.org
+                                if 'image_type' in attachment:
+                                    api_params = {'msgtype': 'm.image', 'body': attachment['title'], 'url': mxcurl}
+                                else: # other files
+                                    api_params = {'msgtype': 'm.file', 'body': attachment['title'], 'url': mxcurl}
+                                response = requests.post(api_endpoint, json=api_params, headers=api_headers_as)
+                                vprint(response.json())
+                            except FileNotFoundError:
+                                api_endpoint = api_base + "_matrix/client/v3/rooms/" + tgtroom + '/send/m.room.message?user_id=' + tgtuser + "&ts=" + str(tgtts) # ts, ?user_id=@_irc_user:example.org
+                                api_params = {'msgtype': 'm.text', 'body': "<<< A file named \"" + attachment['title'] + "\" was lost during the migration to Matrix >>>"}
+                                response = requests.post(api_endpoint, json=api_params, headers=api_headers_as)
+                                vprint(response.json())
+                            if 'description' in attachment:
+                                api_endpoint = api_base + "_matrix/client/v3/rooms/" + tgtroom + '/send/m.room.message?user_id=' + tgtuser + "&ts=" + str(tgtts) # ts, ?user_id=@_irc_user:example.org
+                                api_params = {'msgtype': 'm.text', 'body': attachment['description']}
+                                response = requests.post(api_endpoint, json=api_params, headers=api_headers_as)
+                                vprint(response.json())
+
+                        # if 'image_url' in attachment: # This attachment is an image
+                        #     vprint("an image")
+                        #     api_endpoint = api_base + "/_matrix/media/v3/upload?user_id=" + tgtuser + "&ts=" + str(tgtts)
+                        #     api_params = {'filename': attachment['title']}
+                        #     #files = {'file': open('inputs/files/u5Ga3vn36LCT9bfhW', 'rb')}
+                        #     api_headers_file = api_headers_as
+                        #     api_headers_file['Content-Type'] = attachment['image_type']
+                        #     try:
+                        #         localfile=attachment['image_url']
+                        #         localfile=re.sub("/file-upload/", "", localfile)
+                        #         localfile=re.sub("/.*", "", localfile)
+                        #         with open("inputs/files/" + localfile, 'rb') as f:
+                        #             response = requests.post(api_endpoint, json=api_params, headers=api_headers_file, data=f)
+                        #         vprint(response.json())
+                        #         mxcurl=response.json()['content_uri']
+                        #         api_endpoint = api_base + "_matrix/client/v3/rooms/" + tgtroom + '/send/m.room.message?user_id=' + tgtuser + "&ts=" + str(tgtts) # ts, ?user_id=@_irc_user:example.org
+                        #         api_params = {'msgtype': 'm.image', 'body': attachment['title'], 'url': mxcurl}
+                        #         response = requests.post(api_endpoint, json=api_params, headers=api_headers_as)
+                        #         vprint(response.json())
+                        #     except FileNotFoundError:
+                        #         api_endpoint = api_base + "_matrix/client/v3/rooms/" + tgtroom + '/send/m.room.message?user_id=' + tgtuser + "&ts=" + str(tgtts) # ts, ?user_id=@_irc_user:example.org
+                        #         api_params = {'msgtype': 'm.text', 'body': "<<< An image named \"" + attachment['title'] + "\" was lost during the migration to Matrix >>>"}
+                        #         response = requests.post(api_endpoint, json=api_params, headers=api_headers_as)
+                        #         vprint(response.json())
+                        #     if 'description' in attachment:
+                        #         api_endpoint = api_base + "_matrix/client/v3/rooms/" + tgtroom + '/send/m.room.message?user_id=' + tgtuser + "&ts=" + str(tgtts) # ts, ?user_id=@_irc_user:example.org
+                        #         api_params = {'msgtype': 'm.text', 'body': attachment['description']}
+                        #         response = requests.post(api_endpoint, json=api_params, headers=api_headers_as)
+                        #         vprint(response.json())
+                        elif 'message_link' in attachment: # This is a citation
+                            vprint("A citation")
+                            html = markdown.markdown(currentmsg['msg'])
+                            api_endpoint = api_base + "_matrix/client/v3/rooms/" + tgtroom + '/send/m.room.message?user_id=' + tgtuser + "&ts=" + str(tgtts) # ts, ?user_id=@_irc_user:example.org
+                            api_params = {'msgtype': 'm.text', 'body': "> <" + attachment['author_name'] + ">" + attachment['text'] + "\n\n" + currentmsg['msg'],
+                                "format": "org.matrix.custom.html",
+                                "formatted_body": "<mx-reply><blockquote>In reply to " + attachment['author_name'] + "<br>" + attachment['text'] + "</blockquote></mx-reply>" + html}
+                            response = requests.post(api_endpoint, json=api_params, headers=api_headers_as)
+                            vprint(response.json())
+                            finished=True
+                        else:
+                            exit("Unsupported attachment")
+                # if 'file' in currentmsg: # File upload
+                #     # "file":{"_id":"u5Ga3vn36LCT9bfhW","name":"tree-736885_640.jpg","type":"image/jpeg"}
+                #     api_endpoint = api_base + "/_matrix/media/v3/upload?user_id=" + tgtuser + "&ts=" + str(tgtts)
+                #     api_params = {'filename': currentmsg['file']['name']}
+                #     #files = {'file': open('inputs/files/u5Ga3vn36LCT9bfhW', 'rb')}
+                #     api_headers_file = api_headers_as
+                #     api_headers_file['Content-Type'] = currentmsg['file']['type']
+                #     try:
+                #         with open("inputs/files/" + currentmsg['file']['_id'], 'rb') as f:
+                #             response = requests.post(api_endpoint, json=api_params, headers=api_headers_file, data=f)
+                #         vprint(response.json())
+                #         mxcurl=response.json()['content_uri']
+                #         api_endpoint = api_base + "_matrix/client/v3/rooms/" + tgtroom + '/send/m.room.message?user_id=' + tgtuser + "&ts=" + str(tgtts) # ts, ?user_id=@_irc_user:example.org
+                #         api_params = {'msgtype': 'm.file', 'body': currentmsg['file']['name'], 'url': mxcurl}
+                #         response = requests.post(api_endpoint, json=api_params, headers=api_headers_as)
+                #         vprint(response.json())
+                #     except FileNotFoundError:
+                #         api_endpoint = api_base + "_matrix/client/v3/rooms/" + tgtroom + '/send/m.room.message?user_id=' + tgtuser + "&ts=" + str(tgtts) # ts, ?user_id=@_irc_user:example.org
+                #         api_params = {'msgtype': 'm.text', 'body': "<<< A file named \"" + currentmsg['file']['name'] + "\" was lost during the migration to Matrix >>>"}
+                #         response = requests.post(api_endpoint, json=api_params, headers=api_headers_as)
+                #         vprint(response.json())
+                # else: # standard message
+
+                # Finally post the message
+                if currentmsg['msg'] != "" and not finished:
                     api_endpoint = api_base + "_matrix/client/v3/rooms/" + tgtroom + '/send/m.room.message?user_id=' + tgtuser + "&ts=" + str(tgtts) # ts, ?user_id=@_irc_user:example.org
                     api_params = format_message(currentmsg['msg'])
                     response = requests.post(api_endpoint, json=api_params, headers=api_headers_as)
                     vprint(response.json())
 
-                    if response.status_code == 403 and response.json()['errcode'] == 'M_FORBIDDEN': # not in the room
-                        # Join room : invite then join
-                        api_endpoint = api_base + "/_synapse/admin/v1/join/" + tgtroom
-                        api_params = {'user_id': tgtuser}
-                        response = requests.post(api_endpoint, json=api_params, headers=api_headers_admin)
-                        vprint(response.json())
-                        # api_endpoint = api_base + "_matrix/client/v3/rooms/" + tgtroom + '/invite'
-                        # api_params = {'user_id': tgtuser}
-                        # response = requests.post(api_endpoint, json=api_params, headers=api_headers_admin)
-                        # vprint(response.json())
-                        # api_endpoint = api_base + "_matrix/client/v3/rooms/" + tgtroom + '/join?user_id=' + tgtuser + "&ts=" + str(tgtts)
-                        # api_params = {'msgtype': 'm.text', 'body': 'b' + currentmsg['msg']}
-                        # response = requests.post(api_endpoint, json=api_params, headers=api_headers_as)
-                        # vprint(response.json())
+                if response.status_code == 403 and response.json()['errcode'] == 'M_FORBIDDEN': # not in the room
+                    # Join room : invite then join
+                    api_endpoint = api_base + "/_synapse/admin/v1/join/" + tgtroom
+                    api_params = {'user_id': tgtuser}
+                    response = requests.post(api_endpoint, json=api_params, headers=api_headers_admin)
+                    vprint(response.json())
+                    # api_endpoint = api_base + "_matrix/client/v3/rooms/" + tgtroom + '/invite'
+                    # api_params = {'user_id': tgtuser}
+                    # response = requests.post(api_endpoint, json=api_params, headers=api_headers_admin)
+                    # vprint(response.json())
+                    # api_endpoint = api_base + "_matrix/client/v3/rooms/" + tgtroom + '/join?user_id=' + tgtuser + "&ts=" + str(tgtts)
+                    # api_params = {'msgtype': 'm.text', 'body': 'b' + currentmsg['msg']}
+                    # response = requests.post(api_endpoint, json=api_params, headers=api_headers_as)
+                    # vprint(response.json())
 
-                        # Repost message
-                        api_endpoint = api_base + "_matrix/client/v3/rooms/" + tgtroom + '/send/m.room.message?user_id=' + tgtuser + "&ts=" + str(tgtts) # ts, ?user_id=@_irc_user:example.org
-                        api_params = format_message(currentmsg['msg'])
-                        response = requests.post(api_endpoint, json=api_params, headers=api_headers_as)
-                        vprint(response.json())
+                    # Repost message
+                    api_endpoint = api_base + "_matrix/client/v3/rooms/" + tgtroom + '/send/m.room.message?user_id=' + tgtuser + "&ts=" + str(tgtts) # ts, ?user_id=@_irc_user:example.org
+                    api_params = format_message(currentmsg['msg'])
+                    response = requests.post(api_endpoint, json=api_params, headers=api_headers_as)
+                    vprint(response.json())
 
             else:
                 vprint("not in a room")
-
-# , "event_id": "$143273582443PhrSn:example.org", "origin_server_ts": 1432735824653,  "room_id": "!jEsUZKDJdhlrceRyVU:example.org", "sender": "@example:example.org", "type": "m.room.message", "unsigned": {    "age": 1234  }
-
-#
-#
-# access_token = 'YOURveryLONGaccessTOKENhere'
-# room_id = '!ZkngAyfszzfCqwNZUd:phys:phys.ethz.ch'
-# url = 'https://matrix.phys.ethz.ch/_matrix/client/r0/rooms/' + room_id + '/send/m.room.message'
-# headers = {'Authorization': ' '.join(['Bearer', access_token])}
-# data = {
-#     'body': 'hello matrix',
-#     'format': 'org.matrix.custom.html',
-#     'formatted_body': 'hello <b>matrix</b>',
-#     'msgtype': 'm.text'
-# }
-#
-# r = requests.post(url, json=data, headers=headers)
-
-
-# {
-#   "content": {
-#     "body": "This is an example text message",
-#     "format": "org.matrix.custom.html",
-#     "formatted_body": "<b>This is an example text message</b>",
-#     "msgtype": "m.text"
-#   },
-#   "event_id": "$143273582443PhrSn:example.org",
-#   "origin_server_ts": 1432735824653,
-#   "room_id": "!jEsUZKDJdhlrceRyVU:example.org",
-#   "sender": "@example:example.org",
-#   "type": "m.room.message",
-#   "unsigned": {
-#     "age": 1234
-#   }
-# }
-
-# room types : https://developer.rocket.chat/reference/api/schema-definition/room
-# message types : https://developer.rocket.chat/reference/api/schema-definition/message
